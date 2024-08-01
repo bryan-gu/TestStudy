87714446431b10d736a331c3502457245ab87c87
bryan.gu <371050714@qq.com>
2024-08-02 02:25:11 +0800

    1.创建SwaggerConfig 2.完成Controller层的接口开发

--- Chapter13/src/main/java/com/course/model/User.java
+++ Chapter13/src/main/java/com/course/model/User.java
@@ -7,4 +7,8 @@ public class User {
     private int id;
     private String userName;
     private String password;
+    private String age;
+    private String sex;
+    private String permission;
+    private String isDelete;
 }
