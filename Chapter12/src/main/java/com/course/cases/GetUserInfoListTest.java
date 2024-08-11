2c585c1937a3991619ce33bba8214e26ed0351a9
bryan.gu <371050714@qq.com>
2024-08-12 00:57:37 +0800

    自动化测试代码二次开发（二）

--- Chapter12/src/main/java/com/course/cases/GetUserInfoListTest.java
+++ Chapter12/src/main/java/com/course/cases/GetUserInfoListTest.java
@@ -3,7 +3,13 @@ package com.course.cases;
 import com.course.config.TestConfig;
 import com.course.model.GetUserListCase;
 import com.course.utils.DatabaseUtil;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.util.EntityUtils;
 import org.apache.ibatis.session.SqlSession;
+import org.json.JSONObject;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
@@ -17,5 +23,9 @@ public class GetUserInfoListTest {
         System.out.println(getUserListCase.toString());
         System.out.println(TestConfig.getUserListUrl);
 
+
+        //发送请求，并获取到响应内容
+
+        //比较预期结果和实际结果
     }
 }
